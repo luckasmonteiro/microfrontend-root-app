b004c76d2a21063fd416df01122163201a6f537e
Lucas Monteiro <lucas.siq.monteiro@gmail.com>
2020-10-20 08:50:31 -0300

    Single Spa Workflow

--- Readme.md
+++ Readme.md
@@ -1,9 +1,7 @@
-    // yarn add @babel/core @babel/plugin-syntax-dynamic-import @babel/preset-env @babel/plugin-transform-runtime babel-loader css-loader sass-loader node-sass webpack webpack-cli webpack-dev-server html-webpack-plugin webpack-merge vue-loader file-loader clean-webpack-plugin
-
-    // npm install --save @babel/core @babel/plugin-syntax-dynamic-import @babel/plugin-transform-runtime @babel/preset-env axios babel-loader clean-webpack-plugin css-loader file-loader html-loader html-webpack-plugin mini-css-extract-plugin node-sass sass-loader vue-loader vue-style-loader vue-template-compiler webpack webpack-cli webpack-dev-server webpack-merge 
-
-    // npm install --save-dev vue vue-router
-
-    // npm install --save-dev postcss-loader autoprefixer postcss-import postcss-loader
+    // yarn add -D @babel/core @babel/plugin-syntax-dynamic-import @babel/preset-env @babel/plugin-transform-runtime @babel/plugin-proposal-decorators babel-loader css-loader sass-loader node-sass webpack webpack-cli webpack-dev-server html-webpack-plugin webpack-merge clean-webpack-plugin postcss-loader autoprefixer postcss-import postcss precss mini-css-extract-plugin
 
     // "start": "webpack-dev-server --config ./webpack.dev.js --mode development --host 0.0.0.0",
+
+    // yarn add -D eslint eslint-config-important-stuff eslint-config-prettier eslint-plugin-prettier prettier pretty-quick typescript postcss responsive-loader mini-css-extract-plugin dotenv-webpack precss 
+
+    // yarn add -D html-webpack-plugin@next
---
+++ app/webpack.config.js
@@ -0,0 +1,143 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const merge = require('webpack-merge');
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+
+module.exports = webpackConfigEnv => {
+    const defaultConfig = singleSpaDefaults({
+        // The name of the organization this application is written for
+        orgName: 'name-of-company',
+        // The name of the current project. This usually matches the git repo's name
+        projectName: 'name-of-project',
+        // See https://webpack.js.org/guides/environment-variables/#root for explanation of webpackConfigEnv
+        webpackConfigEnv,
+    });
+
+    return merge(defaultConfig, {
+        entry: path.resolve(__dirname, "src/config.js"),
+        output: {
+            filename: "qualis-mf-root-config.js",
+            libraryTarget: "system",
+            path: path.resolve(__dirname, "dist"),
+        },
+        devtool: "inline-source-map",
+        optimization: {
+            minimize: true,
+        },
+        resolve: {
+            alias: {
+                "@node_modules": path.resolve(__dirname, "node_modules"),
+                "@app": path.resolve(__dirname, "src/"),
+                "@public": path.resolve(__dirname, "public/")
+            },
+            extensions: [
+                "*",
+                ".js",
+                ".jsx",
+                ".scss",
+                ".css",
+                ".ejs"
+            ]
+        },
+        module: {
+            rules: [
+                { parser: { system: false } },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: [
+                                    [
+                                        "@babel/preset-env",
+                                        {
+                                            targets: {
+                                                esmodules: true
+                                            }
+                                        }
+                                    ]
+                                ],
+                            }
+                        }
+                    ],
+                },
+                {
+                    test: /\.(sa|sc)ss$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        // {
+                        //     loader: MiniCssExtractPlugin.loader,
+                        //     options: {
+                        //         esModule: false,
+                        //         modules: {
+                        //             namedExport: false
+                        //         },
+                        //         sourceMap: true,
+                        //         importLoaders: 3,
+                        //         hmr: true
+                        //     }
+                        // },
+                        // {
+                        //     loader: "postcss-loader",
+                        //     options: {
+                        //         sourceMap: true,
+                        //         postcssOptions: {
+                        //             ident: "postcss",
+                        //             plugins: [
+                        //                 require("precss")(),
+                        //                 require("autoprefixer")(),
+                        //                 require("postcss-import")()
+                        //             ]
+                        //         }
+                        //     }
+                        // },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true,
+                                modules: {
+                                    namedExport: false,
+                                    localIdentName: "[local]-[hash:base64:12]"
+                                },
+                                importLoaders: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                },
+            ],
+        },
+        devServer: {
+            historyApiFallback: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            disableHostCheck: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                inject: false,
+                template: "src/index.ejs",
+                isLocal: webpackConfigEnv && webpackConfigEnv.isLocal || true
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "[name].[hash].css",
+                chunkFilename: "[id].[hash].css"
+            }),
+        ],
+        externals: ["single-spa", /^@qualis-mf\/.+$/],
+    })
+}
