1e83d8e99fc488c81ef34b59eeabfbc04e209c4b
Lucas Monteiro <lucas.siq.monteiro@gmail.com>
2020-10-22 16:47:35 -0300

    Some stuff

--- app/src/config.js
+++ app/src/config.js
@@ -2,9 +2,27 @@ import { registerApplication, start } from "single-spa";
 import "@app/styles.scss";
 
 registerApplication({
-  name: "@polyglot-mf/navbar",
+  name: "@qualis-mf-/navbar",
   app: () => System.import("@qualis-mf-navbar"),
   activeWhen: "/",
+});
+
+registerApplication({
+  name: "@qualis-mf-/app1",
+  app: () => System.import("@qualis-mf-app1"),
+  activeWhen: "/app",
+});
+
+registerApplication({
+  name: "@qualis-mf-/app2",
+  app: () => System.import("@qualis-mf-app2"),
+  activeWhen: "/app1",
+});
+
+registerApplication({
+  name: "@qualis-mf-/app3",
+  app: () => System.import("@qualis-mf-app3"),
+  activeWhen: "/app2",
 });
 
 start();
